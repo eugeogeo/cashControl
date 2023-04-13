48373a50a52ecf7dd6c2ddf500d81ed45c8053a8
Geovana <geovanaf1912@gmail.com>
2023-04-13 15:22:14 -0300

    feat: criada base da pagina home

---
+++ src/pages/Home.tsx
@@ -0,0 +1,20 @@
+import { Box, Typography } from "@mui/material";
+
+const Home = () => {
+
+  return (
+    <Box
+    sx={{ 
+      backgroundColor: "#202024",
+      width: "100vw",
+      height: "100vh",
+    }}
+    >
+      <Box>
+        <img src=".//assets/icons/logo.png" />
+      </Box>
+    </Box>
+  );
+}
+
+export default Home;
