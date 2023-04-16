db406ebb6b31c7fca8e130a6fb67a41d264992b9
Geovana <geovanaf1912@gmail.com>
2023-04-16 17:29:35 -0300

    feat: adicionando theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
-import ReactDOM from "react-dom/client";
+import ReactDOM from 'react-dom/client';
 import App from './App';
-import React from "react";
-import { CssBaseline } from "@mui/material";
+import React from 'react';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import theme from './styles/theme';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <CssBaseline />
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <App />
+        </ThemeProvider>
+    </React.StrictMode>,
+);
---
+++ src/styles/theme.ts
@@ -0,0 +1,15 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#015F43',
+    
+        },
+        secondary:{
+            main: '#F75A68',
+        }
+    },
+});
+
+export default theme;
